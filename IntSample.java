c62f05707d08ef23581de5cd87ead44654186dee
Olga Fedorova <bamnu13@gmail.com>
2022-07-25 22:17:01 +0300

    IntSample added

---
+++ IntSample.java
@@ -0,0 +1,10 @@
+public class IntSample {
+    public void math (int x, int y, int z) {
+
+    x = 10;
+    y = 5;
+    z = x / y;
+
+    System.out.println(z);
+    }
+}
--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String hw = "Hello world! Let's try GIT";
         System.out.println(hw);
+
         String ny = "Nice to meet you!";
         System.out.println(ny);
     }
