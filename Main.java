c5dc5e6dae905a729994b63a901e7d61fecfee7b
Olga Fedorova <bamnu13@gmail.com>
2022-07-25 21:04:09 +0300

    String hw

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Let's try GIT");
+        String hw = "Hello world! Let's try GIT";
+        System.out.println(hw);
     }
 }
