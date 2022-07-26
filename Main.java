3f059765af218557e2a7ef9db9085fbedbae7ebc
Olga Fedorova <bamnu13@gmail.com>
2022-07-26 14:33:31 +0300

    New output with Strings hw, ny

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         String hw = "Hello world! Let's try GIT";
-        System.out.println(hw);
 
         String ny = "Nice to meet you!";
-        System.out.println(ny);
+
+        System.out.println(hw + " " + ny);
+
     }
 }
